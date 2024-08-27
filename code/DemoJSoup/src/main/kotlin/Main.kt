415259dd82a30b3569afac89e5d91aef4c8cb6be
2258455 <2258455@cegepmontpetit.ca>
2024-08-27 10:22:14 -0400

    Exercice librairie JSOUP

--- code/DemoJSoup/src/main/kotlin/Main.kt
+++ code/DemoJSoup/src/main/kotlin/Main.kt
@@ -2,9 +2,21 @@ package org.example
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 
 fun main() {
     println("Hello World!")
-    var doc : Document = Jsoup.connect("http://example.com/").get();
+    Jsoup.connect("http://example.com/").get();
+    val doc = Jsoup.connect("https://en.wikipedia.org/").get()
+    println(doc.title())
+    val newsHeadlines: Elements = doc.select("#mp-itn b a")
+    for (headline in newsHeadlines) {
+        println(
+
+            headline.attr("title") + headline.absUrl("href")
+        )
+    }
+
+
 }
