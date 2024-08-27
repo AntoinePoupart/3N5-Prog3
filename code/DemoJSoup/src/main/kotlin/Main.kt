d1795cea2ae4bc44395600a7a1152ba63491e0b6
2258455 <2258455@cegepmontpetit.ca>
2024-08-27 10:14:57 -0400

    JSOUP

---
+++ code/DemoJSoup/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package org.example
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+fun main() {
+    println("Hello World!")
+    var doc : Document = Jsoup.connect("http://example.com/").get();
+}
