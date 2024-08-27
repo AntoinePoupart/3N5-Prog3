745642655526804818348a7bc501a8f880af1055
2258455 <2258455@cegepmontpetit.ca>
2024-08-27 10:15:52 -0400

    Ajouter une librairie dans un projet Kotlin / gradle

--- code/DemoJSoup/src/main/kotlin/Main.kt
+++ code/DemoJSoup/src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.example
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
+
 fun main() {
     println("Hello World!")
     var doc : Document = Jsoup.connect("http://example.com/").get();
