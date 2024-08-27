3cf2d591884987fe7aabf59f294a368be8319657
2258455 <2258455@cegepmontpetit.ca>
2024-08-27 09:21:07 -0400

    Exercice Triangles de taille différente

--- code/BonjourKotlin/src/main/kotlin/Triangles.kt
+++ code/BonjourKotlin/src/main/kotlin/Triangles.kt
@@ -1,9 +1,18 @@
-fun main(args: Array<String>){
-    for(s:String in args){
-        println(s)
-    }
+fun main(){
+    print(triangle(hauteur = 4))
+    print(triangle(hauteur = 8))
+    print(triangle(hauteur = 10))
+    print(triangle(hauteur = 100))
+
 }
 
 fun triangle(hauteur : Int) : String {
-    return "TODO"
+    var maString: String = ""
+    for(i in 1 .. hauteur){
+        for(index in 1..i){
+           maString += "*"
+        }
+        maString += "\n"
+    }
+    return maString
 }
