cc81b71afd754b6aeda141e12d32d2af32dc5867
2258455 <2258455@cegepmontpetit.ca>
2024-09-03 09:07:54 -0400

    Exercice Moyenne

--- code/Moyenne/src/Moyenne.kt
+++ code/Moyenne/src/Moyenne.kt
@@ -1,12 +1,30 @@
+import kotlin.random.Random
+
 fun main(){
-    var tailleTableau = 10;
+   val tableau = creeTableauAleatoire(100)
+    stat(tableau)
+
 }
 
-//
-//fun creeTableauAleatoire(n: Int): Array<Int> {
-//
-//}
 
-fun stat(){
+fun creeTableauAleatoire(n: Int): Array<Int> {
+//val randomValues = List(10) { Random.nextInt(0, 100) }
+//   return  randomValues.toIntArray()
+    return Array(n) { Random.nextInt(0, 101) }
+
+
+}
+
+fun stat(tableau : Array<Int>){
+    val somme = tableau.sum()
+    val moyenne = tableau.average()
+    val min = tableau.min()
+    val max = tableau.max()
+
+//    println("Tableau :  ${tableau.joinToString()}")
+    println("Somme : " + somme)
+    println("Moyenne : " + moyenne)
+    println("Minimum : " + min)
+    println("Maximum : " + max)
 
 }
