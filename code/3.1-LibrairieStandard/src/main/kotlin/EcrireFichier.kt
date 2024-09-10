e8964f873b0497e6b6eae3cc2a9c345ebf2757f8
2258455 <2258455@cegepmontpetit.ca>
2024-09-10 09:22:39 -0400

    Exercice EcrireFichier.kt

---
+++ code/3.1-LibrairieStandard/src/main/kotlin/EcrireFichier.kt
@@ -0,0 +1,24 @@
+package org.example
+
+import java.io.File
+
+fun main(args: Array<String>){
+
+   val fichier = File("vide.txt")
+    if (fichier.createNewFile()) {
+        println("Fichier vide.txt créé avec succès.")
+    }
+    else{
+        println("Le fichier ${fichier} existe déjà")
+    }
+
+
+    val fichierNom = File("../../nom_prenom.txt")
+    val texte = "Poupart Antoine"
+    fichierNom.writeText(texte)
+    println("Le fichier nom_prenom.txt à été créé avec succès.")
+
+
+
+
+}
