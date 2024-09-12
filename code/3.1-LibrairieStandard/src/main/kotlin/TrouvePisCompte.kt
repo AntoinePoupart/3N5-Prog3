431b4f6f39574e523de5f786fdb6af60ba4c2427
2258455 <2258455@cegepmontpetit.ca>
2024-09-12 12:16:23 -0400

    Commencement TrouvePisCompte.kt

---
+++ code/3.1-LibrairieStandard/src/main/kotlin/TrouvePisCompte.kt
@@ -0,0 +1,26 @@
+package main.kotlin
+
+//fun main(args: Array<String>){
+//    val liste1: List<Double> = listOf()
+//    for(i in args){
+//        val number = i.toDouble()
+//        liste1.add(number)
+//    }
+//
+//}
+//
+//fun trouveALaMain(element: Int, liste:List<Int>) : Boolean{
+//return true
+//}
+//
+//fun trouve(element: Int, liste: List<Int>): Boolean{
+//    return  liste.contains(element)
+//}
+//
+//fun compteALaMain(element: Int, liste: List<Int>): Int{
+//    return 1
+//}
+//
+//fun compte(element: Int, liste: List<Int>): Int{
+//    return 3
+//}
