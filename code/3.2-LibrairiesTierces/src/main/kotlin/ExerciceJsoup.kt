c9f02cff5b02437ea67316336fad41de50629d03
Antoine Poupart <antoinepoupart@icloud.com>
2024-09-24 17:45:16 -0400

    Jsoup Terminer

--- code/3.2-LibrairiesTierces/src/main/kotlin/ExerciceJsoup.kt
+++ code/3.2-LibrairiesTierces/src/main/kotlin/ExerciceJsoup.kt
@@ -4,12 +4,22 @@ import org.jsoup.nodes.Element
 import javax.lang.model.util.Elements
 
 fun main(args: Array<String>) {
+    ExerciceJsoup(args)
+    ValidationCourriel()
+}
+
+fun ExerciceJsoup(args: Array<String>) {
     val url = args[0]
     val doc: Document = Jsoup.connect(url).get()
     val link = doc.select("a")
-    for (i in link.indices){
+    for (i in link.indices) {
         println(link[i].attr("href"))
     }
 
 }
 
+fun ValidationCourriel(){
+
+    val listeEmail = listOf("jo@pipo.org", "ma_mu@m.ca", "a.a@a.ca", "a.a@a.aa", "ab@ab", "a.b@ab", "jo")
+
+}
