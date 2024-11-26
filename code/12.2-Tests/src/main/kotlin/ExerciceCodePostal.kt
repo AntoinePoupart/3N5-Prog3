147d4bb4b4fae6a83e12d2d6619b6e3d1792bb99
2258455 <2258455@cegepmontpetit.ca>
2024-11-26 09:23:15 -0500

    Exercice CodePostal

---
+++ code/12.2-Tests/src/main/kotlin/ExerciceCodePostal.kt
@@ -0,0 +1,25 @@
+
+fun main(){
+
+    val codePostal = listOf(
+        "H1Y1E1",
+        "H1Y 1E1",
+        "h3Y 1E8",
+        "H1Y-1E1",
+        "h3Y-1E8",
+        "H1Y  1E1", // Invalide (Trop d'espaces)
+        "H1Y_1E1", // Invalide
+        "h3U 1TO" // Invalide
+    )
+
+
+for(i in codePostal.indices ){
+    val codePostals = codePostal[i]
+    println("Code postal : $codePostals - Valide : ${isValidZipCode(codePostals)}")
+}
+}
+
+fun isValidZipCode(zippy: String): Boolean {
+    val regex = "^[a-zA-Z]\\d[a-zA-Z]([\\s-]?\\d[a-zA-Z]\\d)?$".toRegex()
+    return regex.matches(zippy)
+}
